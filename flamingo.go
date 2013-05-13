b252150228b5bd038e987fa151ba905c4d814f34
mediocregopher <bgp2396@bellsouth.net>
2013-05-13 18:26:08 -0400

    ran through go fmt, then took out tabs. screw that noise

--- flamingo.go
+++ flamingo.go
@@ -1,12 +1,12 @@
 package flamingo
 
 import (
-    "net"
     "fmt"
-    "time"
     "io"
+    "net"
     "strconv"
     "sync"
+    "time"
 )
 
 const CONN_TIMEOUT_CHECK = 10 //seconds
@@ -16,13 +16,13 @@ type Id uint64
 type workerId uint64
 
 type connection struct {
-    conn     *net.Conn
-    cid      Id
+    conn *net.Conn
+    cid  Id
 }
 
 type command interface {
-    ConnId()  Id
-    Type()    int
+    ConnId() Id
+    Type() int
 }
 
 //Types of commands
@@ -32,24 +32,26 @@ const (
 )
 
 type message struct {
-    cid  Id
+    cid Id
     msg []byte
 }
 
 //A message being interpreted as a command is always going to be a write command,
 //the data in the message being what is written
-func (m *message) Type()   int { return WRITE }
-func (m *message) ConnId() Id  { return m.cid  }
+func (m *message) Type() int  { return WRITE }
+func (m *message) ConnId() Id { return m.cid }
 
 type closeCommand Id
-func (c *closeCommand) Type()   int { return CLOSE }
-func (c *closeCommand) ConnId() Id  { return Id(*c) }
+
+func (c *closeCommand) Type() int  { return CLOSE }
+func (c *closeCommand) ConnId() Id { return Id(*c) }
 
 type commandRouter struct {
     sync.RWMutex
     m map[workerId]chan command
 }
-func newCommandRouter() (*commandRouter) {
+
+func newCommandRouter() *commandRouter {
     return &commandRouter{
         m: map[workerId]chan command{},
     }
@@ -62,57 +64,59 @@ type Flamingo struct {
     globalCloseCh chan *connection
 
     //For private use
-    incomingCh   chan *connection
-    cmdR         *commandRouter
+    incomingCh chan *connection
+    cmdR       *commandRouter
 }
 
-func New(port int) (*Flamingo) {
+func New(port int) *Flamingo {
     //Make our listen socket
-    server, err := net.Listen( "tcp", ":" + strconv.Itoa(port) )
-    if server == nil { panic("couldn't start listening: " + err.Error()) }
+    server, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+    if server == nil {
+        panic("couldn't start listening: " + err.Error())
+    }
 
-    flamingo := &Flamingo{ globalOpenCh:  make(chan *connection,2000),
-                           globalReadCh:  make(chan *message,2000),
-                           globalCloseCh: make(chan *connection,2000),
-                           incomingCh:    make(chan *connection),
-                           cmdR:          newCommandRouter() }
+    flamingo := &Flamingo{globalOpenCh: make(chan *connection, 2000),
+        globalReadCh:  make(chan *message, 2000),
+        globalCloseCh: make(chan *connection, 2000),
+        incomingCh:    make(chan *connection),
+        cmdR:          newCommandRouter()}
 
     //Spawn a routine to listen for new connections
-    makeAcceptors(flamingo,server)
+    makeAcceptors(flamingo, server)
 
     go distributor(flamingo)
 
     return flamingo
 }
 
-func (f *Flamingo) RecvOpen() (Id,net.Conn) {
-    c := <- f.globalOpenCh
+func (f *Flamingo) RecvOpen() (Id, net.Conn) {
+    c := <-f.globalOpenCh
     return c.cid, *c.conn
 }
 
-func (f *Flamingo) RecvData() (Id,[]byte) {
-    msg := <- f.globalReadCh
+func (f *Flamingo) RecvData() (Id, []byte) {
+    msg := <-f.globalReadCh
     return msg.cid, msg.msg
 }
 
 func (f *Flamingo) SendData(cid Id, msg []byte) {
-    wrmsg := message{ cid, msg }
-    f.routeCommand(cid,&wrmsg)
+    wrmsg := message{cid, msg}
+    f.routeCommand(cid, &wrmsg)
 }
 
 func (f *Flamingo) RecvClose() Id {
-    c := <- f.globalCloseCh
+    c := <-f.globalCloseCh
     return c.cid
 }
 
 func (f *Flamingo) SendClose(cid Id) {
     cmsg := closeCommand(cid)
-    f.routeCommand(cid,&cmsg)
+    f.routeCommand(cid, &cmsg)
 }
 
 func (f *Flamingo) routeCommand(cid Id, c command) {
     f.cmdR.RLock()
-    f.cmdR.m[ workerId( cid / CONNS_PER_WORKER ) ] <- c
+    f.cmdR.m[workerId(cid/CONNS_PER_WORKER)] <- c
     f.cmdR.RUnlock()
 }
 
@@ -122,18 +126,23 @@ func makeAcceptors(f *Flamingo, listener net.Listener) {
     cidCh := make(chan Id)
     go func() {
         var i Id = 0
-        for { cidCh <- i; i++ }
+        for {
+            cidCh <- i
+            i++
+        }
     }()
 
-
-    for j:=0; j<10; j++ {
+    for j := 0; j < 10; j++ {
         go func() {
             for {
                 client, err := listener.Accept()
-                if client == nil { fmt.Printf("accept failed"+err.Error()+"\n"); continue }
+                if client == nil {
+                    fmt.Printf("accept failed" + err.Error() + "\n")
+                    continue
+                }
 
                 cid := <-cidCh
-                connection := &connection{ &client, cid }
+                connection := &connection{&client, cid}
 
                 f.incomingCh <- connection
                 f.globalOpenCh <- connection
@@ -144,18 +153,18 @@ func makeAcceptors(f *Flamingo, listener net.Listener) {
 }
 
 func distributor(f *Flamingo) {
-    for wid := workerId(0);;wid++ {
-        workerCommandCh  := make(chan command,20)
-        workerIncomingCh := make(chan *connection,20)
+    for wid := workerId(0); ; wid++ {
+        workerCommandCh := make(chan command, 20)
+        workerIncomingCh := make(chan *connection, 20)
 
-        go worker(f,wid,workerIncomingCh,workerCommandCh)
+        go worker(f, wid, workerIncomingCh, workerCommandCh)
         f.cmdR.Lock()
         f.cmdR.m[wid] = workerCommandCh
         f.cmdR.Unlock()
 
         //This works, but I think it's a bottleneck
         for connCount := 0; connCount < CONNS_PER_WORKER; connCount++ {
-            workerIncomingCh <- <- f.incomingCh
+            workerIncomingCh <- <-f.incomingCh
         }
 
         workerIncomingCh <- nil
@@ -164,17 +173,17 @@ func distributor(f *Flamingo) {
 
 func worker(f *Flamingo, wid workerId, incomingCh chan *connection, commandCh chan command) {
 
-    globalReadCh  := f.globalReadCh
+    globalReadCh := f.globalReadCh
 
-    connL := make([]*connection,CONNS_PER_WORKER)
+    connL := make([]*connection, CONNS_PER_WORKER)
     connSlotsUsed := 0
 
     stillInUse := true
 
     for {
-        for i,conn := range connL {
-            if (conn != nil) {
-                err := tryRead(globalReadCh,conn)
+        for i, conn := range connL {
+            if conn != nil {
+                err := tryRead(globalReadCh, conn)
                 if err != nil {
                     (*conn.conn).Close()
                     connL[i] = nil
@@ -186,30 +195,30 @@ func worker(f *Flamingo, wid workerId, incomingCh chan *connection, commandCh ch
 
         for loop := true; loop == true; {
             select {
-                case conn := <-incomingCh:
-                    if conn == nil {
-                        stillInUse = false
-                    } else {
-                        connL[ conn.cid % CONNS_PER_WORKER ] = conn
-                        connSlotsUsed++
-                    }
+            case conn := <-incomingCh:
+                if conn == nil {
+                    stillInUse = false
+                } else {
+                    connL[conn.cid%CONNS_PER_WORKER] = conn
+                    connSlotsUsed++
+                }
 
-                case command := <- commandCh:
-                    ci := command.ConnId() % CONNS_PER_WORKER
-                    switch command.Type() {
-                        case WRITE:
-                            conn := connL[ci]
-                            msg := command.(*message).msg
-                            (*conn.conn).Write(msg)
+            case command := <-commandCh:
+                ci := command.ConnId() % CONNS_PER_WORKER
+                switch command.Type() {
+                case WRITE:
+                    conn := connL[ci]
+                    msg := command.(*message).msg
+                    (*conn.conn).Write(msg)
 
-                        case CLOSE:
-                            conn := connL[ci]
-                            (*conn.conn).Close()
-                            connL[ci] = nil
-                            connSlotsUsed--
-                    }
-                default:
-                    loop = false
+                case CLOSE:
+                    conn := connL[ci]
+                    (*conn.conn).Close()
+                    connL[ci] = nil
+                    connSlotsUsed--
+                }
+            default:
+                loop = false
             }
         }
 
@@ -220,7 +229,7 @@ func worker(f *Flamingo, wid workerId, incomingCh chan *connection, commandCh ch
             //     do we care? At this point all connections should be closed so it's
             //     probably moot
             f.cmdR.Lock()
-            delete(f.cmdR.m,wid)
+            delete(f.cmdR.m, wid)
             f.cmdR.Unlock()
             return
         }
@@ -237,9 +246,8 @@ func tryRead(globalReadCh chan *message, c *connection) error {
     if err == io.EOF {
         return err
     } else if bcount > 0 {
-        msg := &message{ c.cid, buf }
+        msg := &message{c.cid, buf}
         globalReadCh <- msg
     }
     return nil
 }
-
